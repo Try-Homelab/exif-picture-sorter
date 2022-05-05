b0aa55f08a9566910e8f0baba35d91e0f26c5d46
Richard Rossberg <40209986+DerBeleidigende@users.noreply.github.com>
2022-05-05 09:58:19 +0200

    base

--- src/index.js
+++ src/index.js
@@ -1,21 +1,53 @@
-/**
-const express = require('express')
+var ExifImage = require('exif').ExifImage;
+var fs = require('fs');
 var path = require('path')
-const app = express()
-const port = 3000
+var read = require('fs-readdir-recursive')
+var SortString = require('./utils/SortString')
+const config = require('./data/config.json');
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, './web/index.html'));
-  });
+var inputdir = config.inputdir
+var outputdir = config.outputdir
+var sortOutDir = config.folder
+var sortOutName = config.name
+var extension_list = config.extensions
 
-app.get('/test', function (req, res) {
-var level = parseInt(req.query.level) || 0;
-console.log('server level:', level);
-});
-  
-  
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  }) */
+    const files = read(inputdir)
+    files.forEach(function (filename) {
+        console.log(filename)
+        let extension = path.extname(filename).toLowerCase()
+        if(extension_list.includes(extension)){
+            try {
+                new ExifImage({ image : `${inputdir}/${filename}` }, function (error, exifData) {
+                    if (error){
+                        console.log('Error: '+error.message);
+                    }else{
+                        let created = exifData.exif.CreateDate
+                        let date = created.split(" ")
 
-  
+                        let split_created_date = date[0].split(":")
+                        let year = split_created_date[0]
+                        let month = split_created_date[1]
+                        let day = split_created_date[2]
+
+                        let split_created_time = date[0].split(":")
+                        let hour = split_created_time[0]
+                        let minute= split_created_time[1]
+                        let second = split_created_time[2]
+
+                        var dir = SortString.getPath(sortOutDir, year, month, day, hour, minute, second)
+                        var name = SortString.getName(sortOutName, path.parse(path.basename(filename)).name, path.extname(filename).toLowerCase(), year, month, day, hour, minute, second)
+
+                        if (!fs.existsSync(`${outputdir}/${dir}`)){
+                            fs.mkdirSync(`${outputdir}/${dir}`, { recursive: true });
+                        }
+                        fs.copyFile(`${inputdir}/${filename}`, `${outputdir}/${dir}/${name}`, (err) => {
+                            if (err) throw err;
+                            console.log('File Copy Successfully.');
+                        });
+                    }
+                });
+            } catch (error) {
+                console.log('Error: ' + error.message);
+            }
+        }
+    });
