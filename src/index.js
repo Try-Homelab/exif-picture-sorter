fafb6c90fef0faf72cc6443474544b31080fd01c
Try-Homelab <rossberg.richard@gmail.com>
2022-06-03 09:08:37 +0200

    spacers

--- src/index.js
+++ src/index.js
@@ -27,6 +27,8 @@ if(process.env.FOLDER_NAME == "" || process.env.FILE_NAME == ""){
                         console.log('Error: '+error.message);
                     }else{
                         console.log(exifData);
+                        //exif info
+                        //date
                         let created = exifData.exif.CreateDate
                         let date = created.split(" ")
 
@@ -39,9 +41,49 @@ if(process.env.FOLDER_NAME == "" || process.env.FILE_NAME == ""){
                         let hour = split_created_time[0]
                         let minute= split_created_time[1]
                         let second = split_created_time[2]
+                        //-date
 
-                        var dir = SortString.getPath(sortOutDir, year, month, day, hour, minute, second)
-                        var name = SortString.getName(sortOutName, path.parse(path.basename(filename)).name, path.extname(filename).toLowerCase(), year, month, day, hour, minute, second)
+                        //file infos
+                        var file_stats = fs.statSync(`${inputdir}/${filename}`);
+                        var mtime = file_stats.mtime;
+
+                        //original date
+                        let ocreated = exifData.exif.DateTimeOriginal
+                        let odate = ocreated.split(" ")
+
+                        let osplit_created_date = odate[0].split(":")
+                        let oyear = osplit_created_date[0]
+                        let omonth = osplit_created_date[1]
+                        let oday = osplit_created_date[2]
+
+                        let osplit_created_time = odate[0].split(":")
+                        let ohour = osplit_created_time[0]
+                        let ominute= osplit_created_time[1]
+                        let osecond = osplit_created_time[2]
+                        //-original date
+
+                        let make = exifData.image.Make
+                        let model = exifData.image.Model
+                        let software = exifData.image.Software
+                        let iso = exifData.exif.ISO
+                        let shutter = exifData.exif.ShutterSpeedValue
+                        let aperture = exifData.exif.ApertureValue
+                        let max_aperture = exifData.exif.MaxApertureValue
+                        let width = exifData.exif.ExifImageWidth
+                        let height = exifData.exif.ExifImageHeight
+                        let lense = exifData.exif.LensModel
+
+
+
+                        var dir = SortString.getPath(sortOutDir, year, month, day, hour, minute, second,
+                                                     oyear, omonth, oday, ohour, ominute, osecond,
+                                                     make, model, software, iso, shutter, aperture, max_aperture,
+                                                     width, height, lense, mtime)
+                        var name = SortString.getName(sortOutName, path.parse(path.basename(filename)).name, path.extname(filename).toLowerCase(),
+                                                     year, month, day, hour, minute, second,
+                                                     oyear, omonth, oday, ohour, ominute, osecond,
+                                                     make, model, software, iso, shutter, aperture, max_aperture,
+                                                     width, height, lense, mtime)
 
                         if (!fs.existsSync(`${outputdir}/${dir}`)){
                             fs.mkdirSync(`${outputdir}/${dir}`, { recursive: true });
@@ -59,6 +101,108 @@ if(process.env.FOLDER_NAME == "" || process.env.FILE_NAME == ""){
     });
 
     /**
+ * 
+ * {
+
+  image: {
+
+    ImageDescription: '                               ',
+
+    Make: 'SONY',
+
+    Model: 'ILCE-7M2',
+
+    XResolution: 300,
+
+    YResolution: 300,
+
+    ResolutionUnit: 2,
+
+    Software: 'Capture One 22 Macintosh',
+
+    ExifOffset: 204
+
+  },
+
+  thumbnail: { Compression: 6, ThumbnailOffset: 860, ThumbnailLength: 2914 },
+
+  exif: {
+
+    ExposureTime: 0.0125,
+
+    FNumber: 6.3,
+
+    ExposureProgram: 2,
+
+    ISO: 100,
+
+    SensitivityType: 2,
+
+    RecommendedExposureIndex: 100,
+
+    ExifVersion: <Buffer 30 32 33 30>,
+
+    DateTimeOriginal: '2022:05:21 16:33:52',
+
+    CreateDate: '2022:05:21 16:33:52',
+
+    ShutterSpeedValue: 6.321928,
+
+    ApertureValue: 5.310704,
+
+    BrightnessValue: 7.6890625,
+
+    ExposureCompensation: 0,
+
+    MaxApertureValue: 4,
+
+    MeteringMode: 5,
+
+    LightSource: 0,
+
+    Flash: 16,
+
+    FocalLength: 43,
+
+    ExifImageWidth: 3813,
+
+    ExifImageHeight: 5719,
+
+    FileSource: <Buffer 03>,
+
+    SceneType: <Buffer 01>,
+
+    CustomRendered: 0,
+
+    ExposureMode: 0,
+
+    WhiteBalance: 0,
+
+    DigitalZoomRatio: 1,
+
+    FocalLengthIn35mmFormat: 43,
+
+    SceneCaptureType: 0,
+
+    Contrast: 0,
+
+    Saturation: 0,
+
+    Sharpness: 0,
+
+    LensInfo: [ 43, 43, 4, 4 ],
+
+    LensModel: 'Zeiss Vario-Tessar T* FE 24-70 mm F4 ZA OSS (SEL2470Z)'
+
+  },
+
+  gps: {},
+
+  interoperability: {},
+
+  makernote: {}
+
+}
  * 
  * "extensions": [
         ".jpeg",
--- src/utils/SortString.js
+++ src/utils/SortString.js
@@ -1,4 +1,7 @@
-function getPath(sortstring, year, month, day, hour, minute, second) {
+function getPath(sortstring, year, month, day, hour, minute, second,
+                 oyear, omonth, oday, ohour, ominute, osecond,
+                 make, model, software, iso, shutter, aperture, max_aperture,
+                 width, height, lense, mtime) {
 
     var path = 
     sortstring.replace(/%dir%/g, "/")
@@ -8,12 +11,32 @@ function getPath(sortstring, year, month, day, hour, minute, second) {
     .replace(/%hour%/g, hour)
     .replace(/%minute%/g, minute)
     .replace(/%second%/g, second)
+    .replace(/%originalyear%/g, oyear)
+    .replace(/%originalmonth%/g, omonth)
+    .replace(/%originalday%/g, oday)
+    .replace(/%originalhour%/g, ohour)
+    .replace(/%originalminute%/g, ominute)
+    .replace(/%originalsecond%/g, osecond)
+    .replace(/%make%/g, make)
+    .replace(/%model%/g, model)
+    .replace(/%software%/g, software)
+    .replace(/%iso%/g, iso)
+    .replace(/%shutter%/g, shutter)
+    .replace(/%aperture%/g, aperture)
+    .replace(/%maxaperture%/g, max_aperture)
+    .replace(/%width%/g, width)
+    .replace(/%height%/g, height)
+    .replace(/%lense%/g, lense)
+    .replace(/%lastchange%/g, mtime)
 
     return path
 
 }
 
-function getName(sortstring, name, extension, year, month, day, hour, minute, second) {
+function getName(sortstring, name, extension, year, month, day, hour, minute, second,
+                 oyear, omonth, oday, ohour, ominute, osecond,
+                 make, model, software, iso, shutter, aperture, max_aperture,
+                 width, height, lense) {
 
     var path = 
     sortstring.replace(/%dir%/g, "/")
@@ -23,7 +46,24 @@ function getName(sortstring, name, extension, year, month, day, hour, minute, se
     .replace(/%hour%/g, hour)
     .replace(/%minute%/g, minute)
     .replace(/%second%/g, second)
-    .replace(/%filename%/g, name) + extension
+    .replace(/%filename%/g, name)
+    .replace(/%originalyear%/g, oyear)
+    .replace(/%originalmonth%/g, omonth)
+    .replace(/%originalday%/g, oday)
+    .replace(/%originalhour%/g, ohour)
+    .replace(/%originalminute%/g, ominute)
+    .replace(/%originalsecond%/g, osecond)
+    .replace(/%make%/g, make)
+    .replace(/%model%/g, model)
+    .replace(/%software%/g, software)
+    .replace(/%iso%/g, iso)
+    .replace(/%shutter%/g, shutter)
+    .replace(/%aperture%/g, aperture)
+    .replace(/%maxaperture%/g, max_aperture)
+    .replace(/%width%/g, width)
+    .replace(/%height%/g, height)
+    .replace(/%lense%/g, lense)
+    .replace(/%lastchange%/g, mtime) + extension
 
     return path
 
